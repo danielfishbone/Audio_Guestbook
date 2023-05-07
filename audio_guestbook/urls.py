0bb187802077ae9908af5ff4a325e5bf4738a663
DanielSamuels <fishbonedaniel@gmail.com>
2023-05-07 18:06:39 +0100

    added template files

--- audio_guestbook/urls.py
+++ audio_guestbook/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from audio_guestbook import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('settings/', views.settings)
 ]
